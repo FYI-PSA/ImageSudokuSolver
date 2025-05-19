1007558bb32029da129a1a3690b2e1af1a7cf4d2
POPPER <77334085+FYI-PSA@users.noreply.github.com>
2025-05-19 10:00:13 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"  # more precise floating point calculations, more precise model output
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"  # disables warnings and debugging information (it's fine) but still gives errors
 import copy
 import math
 import time
